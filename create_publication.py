27cca2d2bac5524d216db82179d39bebe53b9ed0
humairahussein1 <135341952+humairahussein1@users.noreply.github.com>
2023-07-05 14:35:07 +0000

    Created a spark session and loaded latest data as dataframe

--- create_publication.py
+++ create_publication.py
@@ -12,11 +12,14 @@ For more info on automated excel outputs, find the automated-excel-publications 
 import logging
 import timeit 
 from pathlib import Path
-from src.utils.data_connections import read_sql_file, get_df_from_server, make_database_connection
 from src.utils.file_paths import get_config
 from src.utils.logging_config import configure_logging 
-from src.processing.clean import calculate_years, process_columns
-from src.processing.derive_fields import gp_count_by_region, calculate_mean_years
+
+
+from pyspark.sql import SparkSession
+import requests
+from src.data_ingestion.get_data import download_zip_from_url
+
 
 logger = logging.getLogger(__name__)
 
@@ -24,10 +27,6 @@ def main():
     
     # load config, here we load our project's parameters from the config.toml file
     config = get_config("config.toml") 
-    server = config ['server']
-    database = config['database']
-    schema = config['schema']
-    table = config['table']
     filled_value = config['filled_value']
     output_dir = Path(config['output_dir'])
     log_dir = Path(config['log_dir'])
@@ -36,34 +35,29 @@ def main():
     configure_logging(log_dir, config)
     logger.info(f"Configured logging with log folder: {log_dir}.")
 
-    # sets up database connection
-    conn = make_database_connection(server, database)
-
-    # load data, this part handles importing our data sources     
-    query = read_sql_file('sql', 'example.sql', database, schema, table)
-    gp_df = get_df_from_server(conn, server, database, query)
-
-    # follow pre-processing steps  
-    gp_df.rename(columns={'ADDRESS_LINE_5': 'REGION', 
-                       'OPEN_DATE': 'OPENED', 
-                       'CLOSE_DATE': 'CLOSED'}, inplace=True)
+ 
+    # get artificial HES data as CSV
+    ARTIFICIAL_HES_URL = "https://s3.eu-west-2.amazonaws.com/files.digital.nhs.uk/assets/Services/Artificial+data/Artificial+HES+final/artificial_hes_ae_202302_v1_sample.zip"
+    download_zip_from_url(ARTIFICIAL_HES_URL,overwrite=True)
+ 
     
-    gp_df = process_columns(gp_df, 
-        date_col_names = ['OPENED', 'CLOSED'], 
-        string_col_names= ['REGION', 'NAME']
-        )
+    # create spark session
+    spark = (SparkSession
+        .builder
+        .appName('example_pipeline_pyspark_version')
+        .getOrCreate()
+    )
 
-    gp_df = calculate_years(filled_value, gp_df)
-        
-    # prepare data for CSV
-    publication_breakdowns = {}
-    publication_breakdowns['gp_data'] = gp_df
+    # load latest CSV into dataframe
+    df_hes_data = (spark
+        .read
+        .csv("data/artificial_hes_ae_202302_v1_sample.zip/artificial_hes_ae_202302_v1_sample/artificial_hes_ae_2122.csv", header=True)
+    )
+
 
     # follow data processing steps
-    region_df = gp_count_by_region(gp_df)
-    region_df = calculate_mean_years(region_df, gp_df)
 
-    publication_breakdowns['region_data'] = region_df
+
 
     # produce outputs
     for table_name, df in publication_breakdowns.items():
