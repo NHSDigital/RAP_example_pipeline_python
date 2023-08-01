9cff8e8f01a362d557c199b3b27445b2660fd506
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2023-08-01 14:29:02 +0000

    changed formatting on aggregation functions

--- src/processing/aggregate_counts.py
+++ src/processing/aggregate_counts.py
@@ -1,29 +1,25 @@
 
 from pyspark.sql import functions as F
 
-def get_aggregate_counts(df, counting_col, alias_name):
+def get_aggregate_counts(df_unaggregated, counting_col, alias_name):
     '''
     Function to get counts in dataframe 
     '''
-    df = (df
-        .agg(
-            F.countDistinct(counting_col).alias(alias_name)
-        )
+    df_aggregated = (df_unaggregated
+        .agg(F.countDistinct(counting_col).alias(alias_name))
     )
 
-    return df
+    return df_aggregated
 
 
-def get_grouped_aggregate_counts(df, grouping_col, counting_col, alias_name):
+def get_grouped_aggregate_counts(df_unaggregated, grouping_col, counting_col, alias_name):
     '''
     Function to get counts in dataframe based on grouping
     '''
-    df = (df
+    df_aggregated = (df_unaggregated
         .groupBy(grouping_col)
-        .agg(
-            F.countDistinct(counting_col).alias(alias_name)
-        )
+        .agg(F.countDistinct(counting_col).alias(alias_name))
         .orderBy(grouping_col)
     )
 
-    return df
+    return df_aggregated
