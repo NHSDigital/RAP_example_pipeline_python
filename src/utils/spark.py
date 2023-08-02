6fe823b996eb9b11fd343d96114e46556d7f95fc
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2023-08-02 15:23:26 +0100

    Update src/utils/spark.py added docstring to create_spark_session
    Co-authored-by: Sam Hollings <52575338+SamHollings@users.noreply.github.com>

--- src/utils/spark.py
+++ src/utils/spark.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 
 def create_spark_session(app_name):
+    """Creates a spark session: this is needed to run PySpark code."""
     spark_session = (SparkSession
         .builder
         .appName(app_name)
