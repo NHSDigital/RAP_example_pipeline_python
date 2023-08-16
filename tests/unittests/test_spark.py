66f1a8e78620edd9227d87b05629f12a2b42e0c6
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2023-08-16 10:39:05 +0000

    added spark util test

---
+++ tests/unittests/test_spark.py
@@ -0,0 +1,14 @@
+import pytest
+import pandas
+
+from src.utils import spark as spark_utils
+from pyspark.sql import functions as F
+from pyspark.sql import SparkSession
+
+def test_create_spark_session():
+
+    test_app_name = 'test_app'
+    spark = spark_utils.create_spark_session(test_app_name)
+
+    assert spark.__class__.__name__ == 'SparkSession'
+    assert spark.sparkContext.appName == test_app_name
