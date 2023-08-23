b8d4839e738b65ef6dcbdd53807b502f06823f13
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2023-08-23 10:25:54 +0000

    refactored backtests function

--- tests/backtests/test_compare_outputs.py
+++ tests/backtests/test_compare_outputs.py
@@ -23,35 +23,4 @@ def test_backtests():
 
         print(f"\n Testing file: {ground_truth_file} against {new_output_file}")
 
-        try:
-            # confirm dfs are equal       
-            assert df_output.equals(df_ground_truth)
-            print(f"\nTest status: PASS \nTest Details: CSV ground truth: {ground_truth_file} and CSV output: {new_output_file} are equal.\n")
-        except AssertionError:
-            print(f"Test status: FAILED \nTest Details: {new_output_file} is not equal to {ground_truth_file}.\n")
-
-            # If we get to here, the backtest has failed, but the follow up tests below may help us narrow down
-            # where the difference is coming from:
-            try:
-                # test csvs have the same number of rows and cols       
-                assert df_output.shape == df_ground_truth.shape
-                print(f"\nTest status: PASS \nTest Details: CSV ground truth: {ground_truth_file} and CSV output: {new_output_file} have the same number of rows and cols.\n")
-            except:
-                print(f"Test status: FAILED \nTest Details: Number of rows, cols {df_output.shape} in {new_output_file} is different to {df_ground_truth.shape} in {ground_truth_file}.\n")
-
-            try:
-                #test the columns have the same names and same order
-                assert df_output.columns.tolist() == df_ground_truth.columns.tolist()
-                print(f"Test status: PASS \nTest Details: CSV ground truth: {ground_truth_file} and CSV output: {new_output_file} have the same column names and order.\n")
-            except:
-                print(f"Test status: FAILED \nTest Details: {new_output_file} has different column names to {ground_truth_file}. {df_output.columns.tolist()} compared to {df_ground_truth.columns.tolist()}.\n")
-
-            # test if the contents of each column are the same
-            for col in df_ground_truth.columns:
-                try:
-                    assert (df_ground_truth[col].equals(df_output[col]))
-                    print(f"Test status: PASS \nTest Details: Column {col} in CSV ground truth {ground_truth_file} and CSV output {new_output_file} have the same column contents.\n")
-                except:
-                    print(f'Test status: FAILED \nTest Details: Column comparison: The contents in column {col} are different between {new_output_file} and {ground_truth_file}.') 
-
-            # assert False
+        pd.testing.assert_frame_equal(df_ground_truth, df_output, check_dtype=True)
